1c2f94b8e0ecd4a4e5eec58328e98c392bb86d45
Lucas Sanders <git@butterflyhug.com>
2025-03-30 16:04:26 -0400

    remove extraneous create-react-app boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './styles.css';
+import { useLogStream } from './useLogStream';
 
 export default function App() {
+  const { entries, isLoading } = useLogStream('/cribl.log', fetch);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div>{isLoading ? 'loading...' : 'done!'}</div>
+      {entries.map(({ id, data }) => (
+        <code>
+          <pre key={id}>{JSON.stringify(data)}</pre>
+        </code>
+      ))}
     </div>
   );
 }
